beb4953f8781d92d06de39fa5b23c0c427e02c19
jcr1984 <xps@192.168.0.100>
2014-01-22 21:37:20 -0600

    dandole

---
+++ js/catalogos.js
@@ -0,0 +1,129 @@
+////////////USUARIOS
+function catUsuarios(){
+    $.ajax({
+        data:  '',
+        url:   'catalogos/usuarios/usuarios.html',
+        type:  'post',
+        success:  function (response) {
+            $("#workArea").html(response);
+        }
+    });
+}
+
+function loadAgregarUsuario(){
+    $.ajax({
+        data:  '',
+        url:   'catalogos/usuarios/modalAltaUsuario.html',
+        type:  'post',
+        success:  function (response) {
+            $("#dialogModal").html(response);
+            $("#dialogModal").dialog({
+                autoOpen:false,
+                modal:true,
+                hide:'drop',
+                show:'fold',
+                width: "70%",
+                height: 'auto',
+                position: [50,50],
+                draggable: true,
+                title:'Agregar Usuario',
+                resizable: false
+            });
+            $('#dialogModal').dialog('open');
+        }
+    });
+}
+
+function SelectElement(element,valueToSelect)
+{    
+    var element = document.getElementById(element);
+    element.value = valueToSelect;
+}
+
+function openDetalle(id){
+    var usuario=$("#lblUsuario"+id).html();
+    var nombre=$("#lblNombre"+id).html();
+    var perfil=$("#lblPerfil"+id).html();
+    $.ajax({
+        data:  '',
+        url:   'catalogos/usuarios/modalUpdUsuario.html',
+        type:  'post',
+        success:  function (response) {
+            $("#dialogModal").html(response);
+            $("#txtModUsuario").val(usuario);
+            SelectElement("sltModNombre",id);
+            SelectElement("sltModPerfil",id);
+            
+            $("#dialogModal").dialog({
+                autoOpen:false,
+                modal:true,
+                hide:'drop',
+                show:'fold',
+                width: "70%",
+                height: 'auto',
+                position: [50,50],
+                draggable: true,
+                title:'Modificar Usuario',
+                resizable: false
+            });
+            $('#dialogModal').dialog('open');
+        }
+    });
+}
+///////////CLIENTES
+function catClientes(){
+    $.ajax({
+        data:  '',
+        url:   'catalogos/clientes/clientes.html',
+        type:  'post',
+        success:  function (response) {
+            $("#workArea").html(response);
+        }
+    });
+}
+
+function loadAgregarCliente(){
+}
+
+function openDetalleCliente(id){
+    var cliente=$("#lblCliente"+id).html();
+    var razonSocial=$("#lblRazonSocial"+id).html();
+    var rfcCliente=$("#lblRfcCliente"+id).html();
+    var direccionCliente=$("#lblDireccionCliente"+id).html();
+    $.ajax({
+        data:  '',
+        url:   'catalogos/clientes/modalUpdCliente.html',
+        type:  'post',
+        success:  function (response) {
+            $("#dialogModal").html(response);
+            $("#txtModCliente").val(cliente);
+            $("#txtModRazonSocial").val(razonSocial);
+            $("#txtModRFC").val(rfcCliente);
+            $("#txtModDireccion").val(direccionCliente);
+            
+            $("#dialogModal").dialog({
+                autoOpen:false,
+                modal:true,
+                hide:'drop',
+                show:'fold',
+                width: "40%",
+                height: 'auto',
+                position: [50,50],
+                draggable: true,
+                title:'Modificar Cliente',
+                resizable: false
+            });
+            $('#dialogModal').dialog('open');
+        }
+    });
+    
+}
+
+////////EMPLEADOS
+function catEmpleados(){
+    //TODO
+}
+
+function catProductos(){
+    //TODO
+}
